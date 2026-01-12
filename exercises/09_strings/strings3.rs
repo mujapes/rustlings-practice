6093e0a7c7a195f7e89096dd8780aeecce253c77
mujapes <222509507+mujapes@users.noreply.github.com>
2026-01-12 00:18:11 +0000

    check in 1

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,12 +1,34 @@
 fn trim_me(input: &str) -> &str {
+    let (mut start, mut end) = (0, input.len()-1);
+    for (i, c) in input.char_indices() {
+        if c == ' ' {
+            start = i;
+            break
+        }
+    }
+    for (i, c) in input.char_indices().rev() {
+        if c == ' ' {
+            end = i;
+            break
+        }
+    }
+    &input[start..end+1]
     // TODO: Remove whitespace from both ends of a string.
 }
 
 fn compose_me(input: &str) -> String {
+    format!("{}{}", input, " world!")
     // TODO: Add " world!" to the string! There are multiple ways to do this.
 }
 
-fn replace_me(input: &str) -> String {
+fn replace_me(mut input: &str) -> String {
+    if input != ""return String::from(&input[8..13]);
+    for i in 0..input.len()-4 {
+        if &input[i..i+5] == "cars" {
+            return format!( "{}{}{}", &input[..i+1], "balloons", &input[i+5..] )
+        }
+    }
+    String::from(input)
     // TODO: Replace "cars" in the string with "balloons".
 }
 
